0ebaf089d9577a0160a1dcd0a0aec7c4a93c9d87
sergidb <sergidomenechbalta@gmail.com>
2020-04-08 23:57:11 +0200

    millores control item activitat

--- src/app/components/activity-item/activity-item.component.ts
+++ src/app/components/activity-item/activity-item.component.ts
@@ -9,9 +9,19 @@ import { Activity } from 'src/app/activity';
 export class ActivityItemComponent implements OnInit {
 
   @Input('activity') activity: Activity;
+  @Input('max-users') maxUsers: number = 5;
 
   constructor() { }
 
   ngOnInit() {}
+
+  extraUsersText() {
+    return `+${this.activity.users.length - this.maxUsers}`;
+  }
+  
+  firstUsers() {
+    let qty = this.activity.users.length > this.maxUsers ? this.maxUsers : this.activity.users.length;
+    return this.activity.users.slice(0, qty);
+  }
 
 }
