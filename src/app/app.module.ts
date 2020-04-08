07060068e8e31cffcc63b50057fa988a51f280b4
Albert Nadal Garriga <anadalg@gmail.com>
2020-04-08 10:05:53 +0200

    Implemented class EducfinamentVideoCaptureResult to get and upload a video/thumbnail from the device

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { VideoEditor } from '@ionic-native/video-editor/ngx';
+import { File } from '@ionic-native/file/ngx';
+import { Camera } from '@ionic-native/camera/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -15,14 +18,17 @@ import { HttpClientModule } from "@angular/common/http";
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
-    BrowserModule, 
-    IonicModule.forRoot(), 
+    BrowserModule,
+    IonicModule.forRoot(),
     AppRoutingModule,
     HttpClientModule
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    VideoEditor,
+    File,
+    Camera,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
---
+++ src/environments/cloud.credentials.prod.ts
@@ -0,0 +1,5 @@
+export const cloudCredentials = {
+  region: 'us-west-2',
+  accessKeyId: ACCESS_KEY_ID,
+  secretAccessKey: SECRET_ACCESS_KEY
+};
