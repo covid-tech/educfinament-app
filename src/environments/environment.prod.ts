bf819a29fb863eb80b264ce86222541165fb2ff8
Albert Nadal Garriga <anadalg@gmail.com>
2020-04-09 11:13:53 +0200

    Implemented http client wrapper as a service

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  SERVER_API_URL: 'http://educfinament.albiasoft.com',
+  SERVER_API_COOKIE_DOMAIN: 'educfinament.albiasoft.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  SERVER_API_URL: 'http://educfinament.albiasoft.com',
+  SERVER_API_COOKIE_DOMAIN: 'educfinament.albiasoft.com'
 };
 
 /*
